09dbe7af048b2ea4b530454c1e2c4450962c4d1b
desparzas7902 <desparzas7902@gmail.com>
2024-07-07 15:14:58 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
-})
+console.log("Hello, world!");
