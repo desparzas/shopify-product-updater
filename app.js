1da110b46b98847718664a0e35ccb7c47a24658a
desparzas7902 <desparzas7902@gmail.com>
2024-07-15 01:23:13 -0500

    add morgan

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require("morgan");
 const productRoutes = require("./routes/productRoutes");
 const webhookRoutes = require("./routes/webhookRoutes");
 // create express app
 const app = express();
 
 // Middleware para recibir el cuerpo raw de los webhooks
+app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.raw({ type: "application/json" }));
+
+app.use((req, res, next) => {
+  console.log("Headers de la petición:", req.headers);
+  console.log("Cuerpo de la petición:", req.body);
+
+  // Si buscas el HMAC, asumiendo que viene en los headers
+  const hmac = req.headers["x-shopify-hmac-sha256"]; // Cambia según el nombre del header
+  console.log("HMAC:", hmac);
+  next();
+});
 
 app.use("/api", productRoutes);
 app.use("/webhooks", webhookRoutes);
